f9e1ad10c7c6c75af9bd0da11c5a114dd692c845
lee4339 <skyho4339@naver.com>
2022-04-11 12:53:56 +0900

    JSP 데이터베이스 연결 연습

---
+++ StudySQL/DB연결.sql
@@ -0,0 +1,15 @@
+SELECT
+	*
+FROM
+	user_mst
+WHERE
+	username = 'aaaa';
+	
+	
+SELECT
+	COUNT(um.username) + COUNT(um2.password) AS signin_flag
+FROM
+	user_mst um
+	LEFT OUTER JOIN user_mst um ON(um2.user_code = um.user_code AND um2.password = '1234')
+WHERE
+	um.username = 'aaaa';
