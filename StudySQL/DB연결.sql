5a4b10f54bb15023764d5086c01137e7212c3625
lee4339 <skyho4339@naver.com>
2022-04-12 13:05:58 +0900

    JSP 데이터베이스 연결 연습2

--- StudySQL/DB연결.sql
+++ StudySQL/DB연결.sql
@@ -13,3 +13,14 @@ FROM
 	LEFT OUTER JOIN user_mst um ON(um2.user_code = um.user_code AND um2.password = '1234')
 WHERE
 	um.username = 'aaaa';
+	
+INSERT INTO
+	user_mst(email, NAME, username, PASSWORD, create_date, update_date)
+VALUES(
+	?,
+	?,
+	?,
+	?,
+	NOW(),
+	NOW()
+)
