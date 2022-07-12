bc4462c11aaf5d84de6415d0a3fd9a83ca0e48d8
lee4339 <skyho4339@naver.com>
2022-07-12 12:33:06 +0900

    프로젝트

---
+++ StudySQL/라이브러리프로젝트파일.sql
@@ -0,0 +1,13 @@
+select
+			rental_code,
+			ud.bookcode,
+			bookname,
+			rental_date,
+			return_date,
+			rental_status
+		from
+			rental_book um
+			left outer join book_mst ud on(ud.bookcode = um.bookcode)
+		where
+			rental_status = 1
+	
